e4111424d062d8c4ae4489fece78ebfa5ef201b2
Cintia Mossinelli <cintia.mossinelli@gmail.com>
2026-05-16 15:36:37 -0300

    Cambios en CtaCteCliente

--- TP CAI 1C2026/Program.cs
+++ TP CAI 1C2026/Program.cs
@@ -15,7 +15,7 @@ namespace TP_CAI_1C2026
         static void Main()
         {
             ApplicationConfiguration.Initialize();
-            Application.Run(new ImposicionAgenciaFRM());
+            Application.Run(new CuentaCorrienteClienteFRM());
         }
     }
 }
