6d0a48bdac71fbd5cafac591cd0b11cc4c24c202
julian2511 <julian_jna@hotmail.com>
2026-05-16 15:17:23 -0300

    Cambios EmisionResumenHDRConfirmadasFRM

--- TP CAI 1C2026/Program.cs
+++ TP CAI 1C2026/Program.cs
@@ -4,6 +4,7 @@ using TP_CAI_1C2026.Forms.Entregas.EntregaAgencia;
 using TP_CAI_1C2026.Forms.Entregas.EntregaCD;
 using TP_CAI_1C2026.Forms.Imposicion.ImposicionCD;
 using TP_CAI_1C2026.Forms.UltimaMilla.AdmisionCD;
+using TP_CAI_1C2026.Forms.UltimaMilla.EmisionResumenHDRConfirmadas;
 
 namespace TP_CAI_1C2026
 {
@@ -13,7 +14,7 @@ namespace TP_CAI_1C2026
         static void Main()
         {
             ApplicationConfiguration.Initialize();
-            Application.Run(new CuentaCorrienteClienteFRM());
+            Application.Run(new EmisionResumenHDRConfirmadasFRM());
         }
     }
 }
