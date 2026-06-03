bfd8c4aa077953abef2031c5faa39c32be9d1fdc
Andrés Panitsch <andrespanitsch@hotmail.com>
2026-06-03 20:33:23 -0300

    Minimas correcciones.

---
+++ TP CAI 1C2026/Forms/Almacen/TarifaExtraAlmacen.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+
+namespace TP_CAI_1C2026.Forms.Almacen
+{
+    internal class TarifaExtraAlmacen
+    {
+        private static List<TarifaExtraEntidad> tarifas = new();
+
+        static TarifaExtraAlmacen()
+        {
+            if (File.Exists(@"datos\Tarifas.json"))
+            {
+                string json = File.ReadAllText(@"datos\Tarifas.json");
+                tarifas = JsonSerializer.Deserialize<List<TarifaExtraEntidad>>(json);
+            }
+        }
+
+        public static void Guardar()
+        {
+            string json = JsonSerializer.Serialize(tarifas);
+            File.WriteAllText(@"datos\Tarifas.json", json);
+        }
+    }
+}
---
+++ TP CAI 1C2026/Forms/Almacen/TarifaExtraEntidad.cs
@@ -0,0 +1,8 @@
+﻿namespace TP_CAI_1C2026.Forms.Almacen
+{
+    internal class TarifaExtraEntidad
+    {
+        public TipoExtraEnum Tarifa { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
