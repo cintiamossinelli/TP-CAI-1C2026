5d6d738df6074017cc682f3fdac6bae9269a9da3
lauttascon <lautarotascon@gmail.com>
2026-06-08 23:32:53 -0300

    sadasd

--- TP CAI 1C2026/Forms/Troncal/DespachoHDRTransporte/DespachoHDRTransporteModelo.cs
+++ TP CAI 1C2026/Forms/Troncal/DespachoHDRTransporte/DespachoHDRTransporteModelo.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using TP_CAI_1C2026.Forms.Almacen;
 
 namespace TP_CAI_1C2026.Forms.Troncal.DespachoHDRTransporte
 {
@@ -7,261 +9,80 @@ namespace TP_CAI_1C2026.Forms.Troncal.DespachoHDRTransporte
     {
         internal List<Servicio> ObtenerServicios()
         {
-            return new List<Servicio>
+            var resultado = new List<Servicio>();
+
+            foreach (var hdr in HDRTransporteAlmacen.HDRTransportes)
             {
-                new Servicio
+                var servicioEntidad = ServicioAlmacen.Servicios
+                    .FirstOrDefault(s => s.IdServicio == hdr.IdServicio);
+
+                if (servicioEntidad == null)
                 {
-                    Id = 1,
-                    Empresa = "Flecha Bus",
-                    FechayHora = new DateTime(2026, 6, 1, 8, 0, 0),
-                    GuiasAsociadas = new List<Guias>
-                    {
-                        new() { Id = "AG-1-1", Tamaño = "S", destino = "Rosario" },
-                        new() { Id = "CD-1-123", Tamaño = "S", destino = "Rosario" },
-                        new() { Id = "AG-1-333", Tamaño = "M", destino = "Córdoba" },
-                        new() { Id = "CD-1-2", Tamaño = "M", destino = "Santa Fe" }
-                    }
-                },
-                new Servicio
+                    continue;
+                }
+
+                var empresaEntidad = EmpresaTransporteAlmacen.EmpresasTransporte
+                    .FirstOrDefault(e => e.IdEmpresaTransporte == servicioEntidad.IdEmpresaTransporte);
+
+                var servicio = new Servicio
                 {
-                    Id = 2,
-                    Empresa = "Chevallier",
-                    FechayHora = new DateTime(2026, 6, 1, 10, 30, 0),
-                    GuiasAsociadas = new List<Guias>
+                    Id = hdr.NroHDR,
+                    Empresa = empresaEntidad?.Nombre ?? "Empresa sin identificar",
+                    FechayHora = servicioEntidad.Paradas != null && servicioEntidad.Paradas.Count > 0
+                        ? servicioEntidad.Paradas[0].Fecha
+                        : DateTime.MinValue,
+                    GuiasAsociadas = new List<Guias>()
+                };
+
+                foreach (var nroGuia in hdr.Guias)
+                {
+                    var guiaEntidad = GuiaAlmacen.Guias
+                        .FirstOrDefault(g => g.NroGuia == nroGuia);
+
+                    if (guiaEntidad == null)
                     {
-                        new() { Id = "CC-1-3", Tamaño = "L", destino = "Córdoba" },
-                        new() { Id = "AG-1-4", Tamaño = "S", destino = "Mendoza" },
-                        new() { Id = "AG-1-103", Tamaño = "L", destino = "Rosario" },
-                        new() { Id = "AG-2-123", Tamaño = "XL", destino = "Córdoba" },
+                        servicio.GuiasAsociadas.Add(new Guias
+                        {
+                            Id = nroGuia,
+                            Tamaño = "Sin datos",
+                            destino = "Sin datos"
+                        });
+
+                        continue;
                     }
 
-                },
-                new Servicio
-                {
-                    Id = 3,
-                    Empresa = "Andesmar",
-                    FechayHora = new DateTime(2026, 6, 6, 13, 15, 0),
-                    GuiasAsociadas = new List<Guias>
+                    servicio.GuiasAsociadas.Add(new Guias
                     {
-                        new() { Id = "CD-1-9", Tamaño = "M", destino = "Rosario" },
-                        new() { Id = "CC-1-7", Tamaño = "L", destino = "Santa Fe" },
-                        new() { Id = "CD-2-111", Tamaño = "S", destino = "Rosario" },
-                        new() { Id = "AG-2-12", Tamaño = "M", destino = "Rosario" },
-                    }
+                        Id = guiaEntidad.NroGuia,
+                        Tamaño = guiaEntidad.TipoCaja.ToString(),
+                        destino = ObtenerDestino(guiaEntidad)
+                    });
+                }
 
-                },
-                new Servicio
-                {
-                    Id = 4,
-                    Empresa = "El Rosarino",
-                    FechayHora = new DateTime(2026, 6, 7, 15, 45, 0),
-                    GuiasAsociadas = new List<Guias>
-                    {
-                        new() { Id = "AG-1-5", Tamaño = "S", destino = "Córdoba" },
-                        new() { Id = "CD-1-6", Tamaño = "M", destino = "Mendoza" },
-                        new() { Id = "AG-3-56", Tamaño = "L", destino = "Mendoza" },
-                        new() { Id = "AG-3-1", Tamaño = "XL", destino = "Mendoza" },
-                    }
+                resultado.Add(servicio);
+            }
 
-                },
-                new Servicio
-                {
-                    Id = 5,
-                    Empresa = "Via Bariloche",
-                    FechayHora = new DateTime(2026, 6, 8, 9, 0, 0),
-                    GuiasAsociadas = new List<Guias>
-                    {
-                        new() { Id = "CC-1-8", Tamaño = "L", destino = "Rosario" },
-                        new() { Id = "AG-1-10", Tamaño = "S", destino = "Santa Fe" },
-                        new() { Id = "CC-3-35", Tamaño = "S", destino = "Mendoza" },
-                        new() { Id = "AG-3-20", Tamaño = "M", destino = "Mar del Plata" },
-                    }
+            return resultado;
+        }
 
-                },
-                new Servicio
-                {
-                    Id = 6,
-                    Empresa = "Pullman",
-                    FechayHora = new DateTime(2026, 6, 4, 20, 0, 0),
+        private string ObtenerDestino(GuiaEntidad guia)
+        {
+            if (!string.IsNullOrWhiteSpace(guia.DireccionEntrega))
+            {
+                return guia.DireccionEntrega;
+            }
 
-                    GuiasAsociadas = new List<Guias>
-                    {
-                        new() { Id = "CD-1-11", Tamaño = "M", destino = "Córdoba" },
-                        new() { Id = "CC-1-12", Tamaño = "L", destino = "Mendoza" },
-                        new() { Id = "CC-3-21", Tamaño = "L", destino = "Mar del Plata" },
-                        new() { Id = "AG-3-22", Tamaño = "XL", destino = "Mar del Plata" },
-                    }
-                },
-                new Servicio
-                {
-                    Id = 7,
-                    Empresa = "Plusmar",
-                    FechayHora = new DateTime(2026, 6, 19, 13, 30, 0),
+            if (guia.IdAgenciaEntrega > 0)
+            {
+                return $"Agencia {guia.IdAgenciaEntrega}";
+            }
 
-                    GuiasAsociadas = new List<Guias>
-                    {
-                        new() { Id = "AG-1-13", Tamaño = "S", destino = "Rosario" },
-                        new() { Id = "CD-1-14", Tamaño = "M", destino = "Santa Fe" },
-                        new() { Id = "CD-3-23", Tamaño = "XL", destino = "Olavarria" },
-                        new() { Id = "AG-3-24", Tamaño = "M", destino = "La Pampa" },
-                    }
-                },
-                new Servicio
-                {
-                    Id = 8,
-                    Empresa = "Crucero del Norte",
-                    FechayHora = new DateTime(2026, 6, 17, 13, 15, 0),
+            if (guia.IdCentroDeDistribucionEntrega > 0)
+            {
+                return $"CD {guia.IdCentroDeDistribucionEntrega}";
+            }
 
-                    GuiasAsociadas = new List<Guias>
-                    {
-                        new() { Id = "CC-1-15", Tamaño = "L", destino = "Córdoba" },
-                        new() { Id = "AG-1-16", Tamaño = "S", destino = "Mendoza" },
-                        new() { Id = "AG-3-25", Tamaño = "XL", destino = "Olavarria" },
-                        new() { Id = "CD-3-26", Tamaño = "S", destino = "La Pampa" },
-                    }
-                },
-                new Servicio
-                {
-                    Id = 9,
-                    Empresa = "Balut",
-                    FechayHora = new DateTime(2026, 6, 12, 15, 45, 0),
-
-                    GuiasAsociadas = new List<Guias>
-                    {
-                        new() { Id = "CD-1-17", Tamaño = "M", destino = "Olavarria" },
-                        new() { Id = "CC-1-18", Tamaño = "L", destino = "Mar del Plata" },
-                        new() { Id = "AG-3-259", Tamaño = "XL", destino = "Olavarria" },
-                        new() { Id = "CD-3-267", Tamaño = "S", destino = "La Pampa" },
-                    }
-                },
-                new Servicio
-                {
-                    Id = 10,
-                    Empresa = "El Rápido Argentino",
-                    FechayHora = new DateTime(2026, 6, 21, 9, 0, 0),
-
-                    GuiasAsociadas = new List<Guias>
-                    {
-                        new() { Id = "AG-1-19", Tamaño = "S", destino = "Rosario" },
-                        new() { Id = "CD-1-20", Tamaño = "M", destino = "La Pampa" },
-                        new() { Id = "AG-3-144", Tamaño = "XL", destino = "Olavarria" },
-                        new() { Id = "CD-3-999", Tamaño = "S", destino = "La Pampa" },
-                    }
-                },
-                new Servicio
-                {
-                    Id = 11,
-                    Empresa = "Flecha Bus",
-                    FechayHora = new DateTime(2026, 5, 30, 7, 30, 0),
-                    GuiasAsociadas = new List<Guias>
-                    {
-                        new() { Id = "AG-2-21", Tamaño = "S", destino = "Rosario" },
-                        new() { Id = "CD-2-22", Tamaño = "M", destino = "Santa Fe" },
-                        new() { Id = "AG-3-259", Tamaño = "XL", destino = "Olavarria" },
-                        new() { Id = "CD-3-777", Tamaño = "S", destino = "La Pampa" },
-                    }
-                },
-                new Servicio
-                {
-                    Id = 12,
-                    Empresa = "Chevallier",
-                    FechayHora = new DateTime(2026, 5, 29, 12, 0, 0),
-                    GuiasAsociadas = new List<Guias>
-                    {
-                        new() { Id = "CC-2-23", Tamaño = "L", destino = "Córdoba" },
-                        new() { Id = "AG-2-24", Tamaño = "S", destino = "Mendoza" }
-                    }
-                },
-                new Servicio
-                {
-                    Id = 13,
-                    Empresa = "Andesmar",
-                    FechayHora = new DateTime(2026, 5, 28, 16, 45, 0), // anterior
-                    GuiasAsociadas = new List<Guias>
-                    {
-                        new() { Id = "CD-2-25", Tamaño = "M", destino = "Olavarria" },
-                        new() { Id = "CC-2-26", Tamaño = "L", destino = "Mar del Plata" }
-                    }
-                },
-                new Servicio
-                {
-                    Id = 14,
-                    Empresa = "El Rosarino",
-                    FechayHora = new DateTime(2026, 5, 29, 9, 0, 0),
-                    GuiasAsociadas = new List<Guias>
-                    {
-                        new() { Id = "AG-2-27", Tamaño = "S", destino = "Buenos Aires" },
-                        new() { Id = "CD-2-28", Tamaño = "M", destino = "La Pampa" }
-                    }
-                },
-                new Servicio
-                {
-                    Id = 15,
-                    Empresa = "Via Bariloche",
-                    FechayHora = new DateTime(2026, 5, 30, 8, 15, 0),
-                    GuiasAsociadas = new List<Guias>
-                    {
-                        new() { Id = "CC-2-29", Tamaño = "L", destino = "Rosario" },
-                        new() { Id = "AG-2-30", Tamaño = "S", destino = "Santa Fe" }
-                    }
-                },
-                // Servicios adicionales dentro de los últimos 10 días (para que aparezcan en el filtro)
-                new Servicio
-                {
-                    Id = 16,
-                    Empresa = "Pullman",
-                    FechayHora = new DateTime(2026, 5, 27, 9, 0, 0), // dentro de los últimos 10 días
-                    GuiasAsociadas = new List<Guias>
-                    {
-                        new() { Id = "AG-3-31", Tamaño = "S", destino = "Rosario" },
-                        new() { Id = "CD-3-32", Tamaño = "M", destino = "Cañada de Gómez" }
-                    }
-                },
-                new Servicio
-                {
-                    Id = 17,
-                    Empresa = "Plusmar",
-                    FechayHora = new DateTime(2026, 5, 28, 14, 30, 0), // dentro de los últimos 10 días
-                    GuiasAsociadas = new List<Guias>
-                    {
-                        new() { Id = "CC-3-33", Tamaño = "L", destino = "Córdoba" },
-                        new() { Id = "AG-3-34", Tamaño = "S", destino = "Mendoza" }
-                    }
-                },
-                new Servicio
-                {
-                    Id = 18,
-                    Empresa = "Crucero del Norte",
-                    FechayHora = new DateTime(2026, 5, 29, 18, 0, 0), // dentro de los últimos 10 días
-                    GuiasAsociadas = new List<Guias>
-                    {
-                        new() { Id = "CD-3-35", Tamaño = "M", destino = "Olavarria" },
-                        new() { Id = "CC-3-36", Tamaño = "L", destino = "Mar del Plata" }
-                    }
-                },
-                new Servicio
-                {
-                    Id = 19,
-                    Empresa = "Balut",
-                    FechayHora = new DateTime(2026, 5, 7, 7, 45, 0), // límite inferior (hace 10 días)
-                    GuiasAsociadas = new List<Guias>
-                    {
-                        new() { Id = "AG-3-37", Tamaño = "S", destino = "Buenos Aires" },
-                        new() { Id = "CD-3-38", Tamaño = "M", destino = "La Pampa" }
-                    }
-                },
-                new Servicio
-                {
-                    Id = 20,
-                    Empresa = "El Rápido Argentino",
-                    FechayHora = new DateTime(2026, 5, 16, 11, 15, 0), // dentro de los últimos 10 días
-                    GuiasAsociadas = new List<Guias>
-                    {
-                        new() { Id = "CC-3-39", Tamaño = "L", destino = "Rosario" },
-                        new() { Id = "AG-3-40", Tamaño = "S", destino = "Santa Fe" }
-                    }
-                },
-            };
+            return "Sin destino";
         }
     }
 }
--- TP CAI 1C2026/Forms/UltimaMilla/EmisionResumenHDR/EmisionResumenHDRModelo.cs
+++ TP CAI 1C2026/Forms/UltimaMilla/EmisionResumenHDR/EmisionResumenHDRModelo.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
+using TP_CAI_1C2026.Forms.Almacen;
 
 namespace TP_CAI_1C2026.Forms.UltimaMilla.EmisionResumenHDR
 {
@@ -14,31 +16,27 @@ namespace TP_CAI_1C2026.Forms.UltimaMilla.EmisionResumenHDR
                 return null;
             }
 
-            var cuitFormateado = NormalizarCuit(text);
-            if (cuitFormateado == null) // es que no es válido
+            var dniNormalizado = NormalizarCuit(text);
+            if (dniNormalizado == null)
             {
                 MessageBox.Show("El DNI del fletero debe ser un número válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return null;
             }
 
-            // Simulación de búsqueda en una base de datos o servicio
-            var FleterosSimulados = new List<Fletero>
-            {
-                new Fletero { Dni = "32443521", Nombre = "Jorge Fernandez" },
-                new Fletero { Dni = "33678891", Nombre = "Tomas Sanchez" },
-                new Fletero { Dni = "29535430", Nombre = "Yanina Mossinelli" },
-                new Fletero { Dni = "40802312", Nombre = "José Perez" },
-                new Fletero { Dni = "30461832", Nombre = "Juan Gonzalez" }
-            };
+            var fleteroEntidad = FleteroAlmacen.Fleteros
+                .FirstOrDefault(f => f.DNI.ToString() == dniNormalizado);
 
-            var fleteroEncontrado = FleterosSimulados.Find(c => c.Dni == cuitFormateado);
-            if (fleteroEncontrado == null)
+            if (fleteroEntidad == null)
             {
-                MessageBox.Show($"No se encontró un fletero con DNI {cuitFormateado}.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show($"No se encontró un fletero con DNI {dniNormalizado}.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return null;
             }
 
-            return fleteroEncontrado;
+            return new Fletero
+            {
+                Dni = fleteroEntidad.DNI.ToString(),
+                Nombre = fleteroEntidad.Nombre
+            };
         }
 
         public static string? NormalizarCuit(string? texto)
@@ -48,7 +46,6 @@ namespace TP_CAI_1C2026.Forms.UltimaMilla.EmisionResumenHDR
                 return null;
             }
 
-            // Dejar solo números
             string dni = new string(texto.Where(char.IsDigit).ToArray());
 
             if (!long.TryParse(dni, out _))
@@ -60,54 +57,36 @@ namespace TP_CAI_1C2026.Forms.UltimaMilla.EmisionResumenHDR
             {
                 return null;
             }
-            else
-            {
-                return dni;
-            }
-        }
-      
-        private List<HDREntrega> EntregasSimuladas = new List<HDREntrega>
-        {
-            new HDREntrega(1001, "Av. Corrientes 1234, CABA", 5, "32443521"),
-            new HDREntrega(1002, "Calle Florida 450, CABA", 3, "32443521"),
-            new HDREntrega(1003, "Av. Santa Fe 2900, CABA", 8, "33678891"),
-            new HDREntrega(1004, "Brandsen 805, CABA", 4, "29535430")
-        };
 
-        private List<HDRRetiro> RetirosSimuladas = new List<HDRRetiro>
-        {
-            new HDRRetiro(5001, "Depósito Central Barracas", 2, "32443521"),
-            new HDRRetiro(5002, "Sucursal Flores, Rivadavia 7000", 6, "33678891"),
-            new HDRRetiro(5003, "Sucursal Belgrano, Cabildo 2200", 1, "33678891")
-        };
+            return dni;
+        }
 
         public List<HDREntrega> BuscarEntregasPorDni(string dni)
         {
-            List<HDREntrega> resultado = new List<HDREntrega>();
-            foreach (var entrega in EntregasSimuladas)
-            {
-                if (entrega.DniFleteroAsignado == dni)
-                {
-                    resultado.Add(entrega);
-                }
-            }
-            return resultado;
+            return HDREntregaAlmacen.HDREntregas
+                .Where(e => e.DNIFletero.ToString() == dni)
+                .Select(e => new HDREntrega(
+                    e.NroHDR,
+                    e.Domicilio,
+                    e.CantEncomiendas,
+                    e.DNIFletero.ToString()
+                ))
+                .ToList();
         }
 
         public List<HDRRetiro> BuscarRetirosPorDni(string dni)
         {
-            List<HDRRetiro> resultado = new List<HDRRetiro>();
-            foreach (var retiro in RetirosSimuladas)
-            {
-                if (retiro.DniFleteroAsignado == dni)
-                {
-                    resultado.Add(retiro);
-                }
-            }
-            return resultado;
+            return HDRRetiroAlmacen.HDRRetiros
+                .Where(r => r.DniFletero.ToString() == dni)
+                .Select(r => new HDRRetiro(
+                    r.NroHDR,
+                    r.Domicilio,
+                    r.CantEncomiendas,
+                    r.DniFletero.ToString()
+                ))
+                .ToList();
         }
 
-        // Devuelve ambos listados filtrados por DNI en una sola llamada
         public (List<HDREntrega> entregas, List<HDRRetiro> retiros) ObtenerEntregasYRetirosPorDni(string dni)
         {
             var entregas = BuscarEntregasPorDni(dni);
@@ -115,7 +94,6 @@ namespace TP_CAI_1C2026.Forms.UltimaMilla.EmisionResumenHDR
             return (entregas, retiros);
         }
 
-        // Intenta obtener entregas y retiros por DNI; si no hay ninguno devuelve false y un mensaje
         public bool TryObtenerEntregasYRetirosPorDni(string dni, out List<HDREntrega> entregas, out List<HDRRetiro> retiros, out string mensaje)
         {
             entregas = BuscarEntregasPorDni(dni);
@@ -132,4 +110,3 @@ namespace TP_CAI_1C2026.Forms.UltimaMilla.EmisionResumenHDR
         }
     }
 }
-
