35095cc4a03588d9b723f3e563f5b14c69489541
Martín Nemi <martinnemi4@gmail.com>
2026-06-11 23:05:01 -0300

    Modificaciones txt

--- TP CAI 1C2026/Program.cs
+++ TP CAI 1C2026/Program.cs
@@ -28,7 +28,7 @@ namespace TP_CAI_1C2026
         static void Main()
         {
             ApplicationConfiguration.Initialize();
-            Application.Run(new EmisionHDRTransporteFRM());
+            Application.Run(new ResultadoCostosVentasFRM());
         }
     }
 }
